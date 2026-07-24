610490553f40c1bdaed5d85fcdf49f8aa33424cc
ltlstv <ltlstv420@gmail.com>
2026-07-25 01:51:50 +0300

    preA_0.03 websocket

---
+++ src/main/java/org/example/dto/imgDto/ImgDownload.java
@@ -0,0 +1,5 @@
+package org.example.dto.imgDto;
+
+public class ImgDownload {
+    
+}
---
+++ src/main/java/org/example/restControllers/ImgController.java
@@ -0,0 +1,13 @@
+package org.example.restControllers;
+
+import lombok.RequiredArgsConstructor;
+import org.example.dto.imgDto.ImgUpload;
+import org.example.dto.imgDto.ImgDownload;
+import org.example.services.AuthService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/img")
+public class ImgController {
+    
+}
