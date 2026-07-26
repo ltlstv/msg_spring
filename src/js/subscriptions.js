54991e9741a800565feef2a3f708d40479857aef
Егор Эннс <ennsegor@gmail.com>
2026-07-26 09:49:05 +0700

    preA_0.04 websocket(fixed WebSocket message output)

--- src/js/subscriptions.js
+++ src/js/subscriptions.js
@@ -1,6 +1,12 @@
+import { renderSingleMessage } from './get_messages_history.js';
+
 export function subscribeToMessages(stompClient) {
   stompClient.subscribe('/user/queue/messages', (message) => {
-    const data = JSON.parse(message.body);
-    console.log(data);
+    const item = JSON.parse(message.body).message;
+
+    const container = document.getElementById('message-container');
+    if (!container) return;
+
+    renderSingleMessage({ sender: item.sender, message: item.message }, container);
   });
 }
